459176394ef82d9c98b321906ddf0933d1c5997c
Gishandamindu <gishankalasinghe1999@gmail.com>
2025-01-13 16:08:53 +0530

    Update nextjs.yml file

--- myportfolio/next.config.mjs
+++ myportfolio/next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     output: 'export',
-    trailingSlash: true,
     images: {
       unoptimized: true,
     },
+    assetPrefix: './',
+    basePath: '',
   };
   
   export default nextConfig;
-  
   
   
