67a7945177d416b5436b6bb8bf9f79b947b34a20
Gishandamindu <gishankalasinghe1999@gmail.com>
2025-01-13 17:01:30 +0530

    Add env varables

--- myportfolio/next.config.mjs
+++ myportfolio/next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
     output: 'export',
     images: {
       unoptimized: true,
     },
-    assetPrefix: './',
-    basePath: '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/My-Portfolio' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/My-Portfolio' : '',
   };
   
   export default nextConfig;
-  
   
